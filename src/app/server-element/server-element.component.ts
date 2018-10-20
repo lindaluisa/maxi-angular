de9545e6e43c5b0cb2e75dda131ffe2656d28aa2
lindaluisa <lindaluisarosenthal@gmail.com>
2018-10-20 17:17:04 +0200

    implementing ng-content, adding different lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ViewEncapsulation,
+  ElementRef,
+  AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-server-element',
@@ -7,13 +15,23 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
 
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
+export class ServerElementComponent implements OnInit, AfterViewInit {
 
+  // tslint:disable-next-line:no-input-rename
+  @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: String;
+  @ViewChild('heading') header: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('ngOnInit called');
+    console.log('Text Content ' + this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+    console.log('Text Content ' + this.header.nativeElement.textContent);
   }
 
 }
