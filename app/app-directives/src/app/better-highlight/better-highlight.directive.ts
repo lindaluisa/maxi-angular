788332f1ec3728016659a04beb381a680ba005f0
Linda Luisa Rosenthal <linda.luisa.rosenthal@everis.com>
2018-10-22 19:18:49 +0200

    adding customized directive to display color

--- app/app-directives/src/app/better-highlight/better-highlight.directive.ts
+++ app/app-directives/src/app/better-highlight/better-highlight.directive.ts
@@ -4,14 +4,17 @@ import {
   ElementRef, 
   OnInit, 
   HostListener, 
-  HostBinding
+  HostBinding,
+  Input
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor: string = "transparent"; 
+  @Input() defaultColor: string = 'transparent';
+  @Input('appBetterHighlight') highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string = this.highlightColor; 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
   
   ngOnInit() {
@@ -20,12 +23,12 @@ export class BetterHighlightDirective implements OnInit {
 
   @HostListener('mouseenter') mouseover(eventDate: Event) {
 /*     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');*/  
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
 }
 
   @HostListener('mouseleave') mouseleave(eventDate: Event) {
 /*     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');*/  
-  this.backgroundColor = 'transparent';
+  this.backgroundColor = this.defaultColor;
 }
 
 }
