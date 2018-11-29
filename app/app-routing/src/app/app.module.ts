5ebd27c5f694269c3c4c49e26f8c53916ce633e6
lindaluisa <lindaluisarosenthal@gmail.com>
2018-11-29 08:36:59 +0100

    preparing id routing, commented out code

--- app/app-routing/src/app/app.module.ts
+++ app/app-routing/src/app/app.module.ts
@@ -17,7 +17,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UserComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent },
   { path: 'servers', component: ServerComponent }
 ];
 
