0442a84328d49767a47fd51815a5ff6171cec684
Linda Luisa Rosenthal <linda.luisa.rosenthal@everis.com>
2019-01-21 15:47:32 +0100

    binding to signupForm and accesing form via formControlName

--- app/forms-reactive-start/src/app/app.component.ts
+++ app/forms-reactive-start/src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms'
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({});
+  }
 }
